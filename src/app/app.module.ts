94ffd46d96f9d597233c71c5f8b1d0e734bfbeb8
Michał Ligęza <mligeziak@gmail.com>
2017-12-17 17:23:37 +0100

    add debounce time to search form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +20,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     HttpClientModule,
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,21 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ApiService } from '../../services/api.service';
+import "rxjs/add/operator/debounceTime";
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  public query: string = '';
+export class HomeComponent implements OnInit {
   public results;
+  public queryFormControl = new FormControl();
 
   constructor(public apiService: ApiService) {
   }
 
-  public search() {
-    this.apiService.search(this.query).subscribe((response) => {
-      this.results = response;
-    });
+  ngOnInit() {
+    this.queryFormControl.valueChanges
+      .debounceTime(400)
+      .subscribe(value => this.search(value));
+  }
+
+  public search(value: string) {
+    this.apiService.search(value)
+      .subscribe((response) => {
+        this.results = response;
+      });
   }
 }
